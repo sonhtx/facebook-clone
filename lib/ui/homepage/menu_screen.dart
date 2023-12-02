b6a1de9ee5648557a59a937461b2d156caf8c0ee
Dũng Tống Việt <vietdung282002@gmail.com>
2023-12-02 15:51:01 +0700

    fix menu

--- lib/ui/homepage/menu_screen.dart
+++ lib/ui/homepage/menu_screen.dart
@@ -1,7 +1,8 @@
-import 'package:anti_fb/widgets/ButtonWidget.dart';
 import 'package:anti_fb/widgets/card_item.dart';
 import 'package:flutter/material.dart';
+
 import '../../constants/constants.dart';
+import '../../widgets/ButtonWidget.dart';
 import '../../widgets/circle_button.dart';
 
 class MenuTab extends StatelessWidget {
@@ -13,102 +14,102 @@ class MenuTab extends StatelessWidget {
     return Scaffold(
       backgroundColor: Constants.bg,
       body: SafeArea(
-        child: NestedScrollView(
-            headerSliverBuilder:
-                (BuildContext context, bool innerBoxIsScrolled) => [
-                      SliverAppBar(
-                        backgroundColor: Constants.bg,
-                        title: const Text('Menu',
-                            style: TextStyle(
-                                fontSize: 25.0, fontWeight: FontWeight.bold)),
-                        centerTitle: false,
-                        floating: true,
-                        actions: [
-                          CircleButton(
-                              icon: Icons.search,
-                              iconSize: 30.0,
-                              onPressed: () {})
-                        ],
-                      )
-                    ],
-            body: Column(
-              children: [
-                const SizedBox(
-                  height: 10,
-                ),
-                CardItem(
-                  width: screenWidth - 20.0,
-                  height: 100.0,
-                  label: 'Messi',
-                  widget: const CircleAvatar(
-                    backgroundImage:
-                        AssetImage('assets/images/messi-world-cup.png'),
-                    radius: 35.0,
-                  ),
-                  onPressed: () {},
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                ExpandTab(
-                  image: const Icon(
-                    Icons.help_outline,
-                    size: 30.0,
-                  ),
-                  title: 'Help & Support',
-                  card: CardItem(
-                    width: screenWidth - 20.0,
-                    height: 70.0,
-                    label: "Terms & Policies",
-                    widget: const Icon(
-                      Icons.policy_outlined,
-                      size: 30.0,
-                    ),
-                    onPressed: () {},
-                  ),
-                ),
-                ExpandTab(
-                    title: "Setting and privacy",
-                    card: CardItem(
+          child: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                  backgroundColor: Constants.bg,
+                  title: const Text('Menu',
+                  style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold)),
+                  centerTitle: false,
+                  floating: true,
+                  pinned: true,
+                  actions: [
+                    CircleButton(
+                        icon: Icons.search, iconSize: 30.0, onPressed: () {}),
+              ]),
+              SliverToBoxAdapter(
+                child: Container(
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 10,
+                      ),
+                    CardItem(
                       width: screenWidth - 20.0,
-                      height: 70.0,
-                      label: "Settings",
-                      widget: const Icon(
-                        Icons.person,
-                        size: 30.0,
+                      height: 100.0,
+                      label: 'Messi',
+                      widget: const CircleAvatar(
+                        backgroundImage:
+                            AssetImage('assets/images/messi-world-cup.png'),
+                        radius: 35.0,
                       ),
                       onPressed: () {},
                     ),
-                    image: const Icon(
-                      Icons.settings,
-                      size: 30.0,
-                    )),
-                const SizedBox(
-                  height: 20,
-                ),
-                ButtonWidget(
-                    width: screenWidth - 20.0,
-                    fontSize: 13.0,
-                    buttonText: "Logout",
-                    paddingTop: 15.0,
-                    textColor: Colors.black,
-                    backgroundColor: Constants.btnbg,
-                    borderColor: Constants.bg,
-                    radius: 8.0,
-                    onPressed: () {}),
-                ButtonWidget(
-                    width: screenWidth - 20.0,
-                    fontSize: 13.0,
-                    buttonText: "Exit",
-                    paddingTop: 15.0,
-                    textColor: Colors.black,
-                    backgroundColor: Constants.btnbg,
-                    borderColor: Constants.bg,
-                    radius: 8.0,
-                    onPressed: () {}),
-              ],
-            )),
-      ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ExpandTab(
+                      image: const Icon(
+                        Icons.help_outline,
+                        size: 30.0,
+                      ),
+                      title: 'Help & Support',
+                      card: CardItem(
+                        width: screenWidth - 20.0,
+                        height: 65 - .0,
+                        label: "Terms & Policies",
+                        widget: const Icon(
+                          Icons.policy_outlined,
+                          size: 30.0,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    ExpandTab(
+                        title: "Setting and privacy",
+                        card: CardItem(
+                          width: screenWidth - 20.0,
+                          height: 65.0,
+                          label: "Settings",
+                          widget: const Icon(
+                            Icons.person,
+                            size: 30.0,
+                          ),
+                          onPressed: () {},
+                        ),
+                        image: const Icon(
+                          Icons.settings,
+                          size: 30.0,
+                        )),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ButtonWidget(
+                        width: screenWidth - 20.0,
+                        fontSize: 13.0,
+                        buttonText: "Logout",
+                        paddingTop: 15.0,
+                        textColor: Colors.black,
+                        backgroundColor: Constants.btnbg,
+                        borderColor: Constants.bg,
+                        radius: 8.0,
+                        onPressed: () {}),
+                    ButtonWidget(
+                        width: screenWidth - 20.0,
+                        fontSize: 13.0,
+                        buttonText: "Exit",
+                        paddingTop: 15.0,
+                        textColor: Colors.black,
+                        backgroundColor: Constants.btnbg,
+                        borderColor: Constants.bg,
+                        radius: 8.0,
+                        onPressed: () {}),
+                ],
+              ),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
