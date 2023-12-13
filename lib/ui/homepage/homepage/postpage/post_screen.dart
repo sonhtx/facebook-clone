9fdde95e1bd0d733a2d87faa73ec976f8d6685bf
Dũng Tống Việt <vietdung282002@gmail.com>
2023-12-13 23:04:37 +0700

    post_screen

--- lib/ui/homepage/homepage/postpage/post_screen.dart
+++ lib/ui/homepage/homepage/postpage/post_screen.dart
@@ -42,7 +42,9 @@ class PostScreen extends StatelessWidget{
           padding: const EdgeInsets.only(left: 10, right: 10),
           child: Column(
             children: [
-              Text(described),
+              Align(alignment: Alignment.topLeft,
+                  child: Text(described)),
+
               (images.isNotEmpty)
                   ? ImageWidget(images: images)
               // SizedBox(
