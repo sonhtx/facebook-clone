a1705ebb1623adfe69bc3e0a4877c0291d9de68e
T. X. Son Hoang <cr11htxuanson@gmail.com>
2024-01-02 17:13:00 +0700

    complete friend profile

--- lib/api/profile/userinfo_api.dart
+++ lib/api/profile/userinfo_api.dart
@@ -37,4 +37,23 @@ class UserInfoApi {
       return null; // get list false
     }
   }
+
+  Future<int> getFriendStatus(String user_id) async {
+    await _initializeHeaders();
+    final Map<String, dynamic> requestBody = {
+      "user_id": user_id,
+    };
+    final response = await http.post(
+      Uri.parse('$apiUrl/get_user_info'),
+      headers: headers,
+      body: json.encode(requestBody),
+    );
+    final jsonResponse = json.decode(response.body);
+    if (jsonResponse['code'] == '3001') {
+      return 4;
+    } else if (jsonResponse['code'] == '1000') {
+      return int.parse(jsonResponse['data']['is_friend']);
+    }
+    return -1;
+  }
 }
